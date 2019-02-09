caa73243fb2ef6a971fc1eb3ba67d358064d6a23
Jakub Bibro <jbibro@gmail.com>
2019-02-09 14:11:37 +0100

    nats

--- src/main/java/com/github/jbibro/rsocketchat/server/ChatServer.java
+++ src/main/java/com/github/jbibro/rsocketchat/server/ChatServer.java
@@ -1,39 +1,45 @@
 package com.github.jbibro.rsocketchat.server;
 
+import java.io.IOException;
+
+import io.nats.client.Connection;
+import io.nats.client.Nats;
 import io.rsocket.AbstractRSocket;
 import io.rsocket.Payload;
 import io.rsocket.RSocketFactory;
 import io.rsocket.transport.netty.server.TcpServerTransport;
 import io.rsocket.util.DefaultPayload;
 import lombok.extern.slf4j.Slf4j;
-import org.reactivestreams.Publisher;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.FluxSink;
 import reactor.core.publisher.Mono;
-import reactor.core.publisher.ReplayProcessor;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 @Slf4j
 public final class ChatServer {
-    public static void main(String[] args) {
-        ReplayProcessor<String> replyProcessor = ReplayProcessor.create();
-        Flux<String> flux = replyProcessor.replay(10).autoConnect();
-        FluxSink<String> sink = replyProcessor.sink();
-
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Connection nc = Nats.connect();
         RSocketFactory.receive()
             .acceptor((setup, sendingSocket) -> {
                 AbstractRSocket rSocket = new AbstractRSocket() {
                     @Override
-                    public Flux<Payload> requestChannel(Publisher<Payload> payloads) {
-                        Flux
-                            .from(payloads)
-                            .map(Payload::getDataUtf8)
-                            .subscribe(sink::next);
-                        return flux.map(DefaultPayload::create);
+                    public Mono<Void> fireAndForget(Payload payload) {
+                        nc.publish("chat", payload.getData().array());
+                        return Mono.empty();
+                    }
+
+                    @Override
+                    public Flux<Payload> requestStream(Payload payload) {
+                        return Flux.<String>push(sink ->
+                            nc.createDispatcher(msg ->
+                                sink.next(new String(msg.getData(), UTF_8))).subscribe("chat")
+                        )
+                            .map(DefaultPayload::create);
                     }
                 };
                 return Mono.just(rSocket);
             })
-            .transport(TcpServerTransport.create("0.0.0.0", 8080))
+            .transport(TcpServerTransport.create(8081))
             .start()
             .block()
             .onClose()
